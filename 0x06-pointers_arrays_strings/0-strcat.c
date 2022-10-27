e1b55f5d6e7cf5034ed3de73c3df6e7d04957218
Hassan Omosomi Ann <hassanomosomi@gmail.com>
2022-10-27 14:22:28 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+include "main.h"
+char *_strcat(char *dest, char *src)
+{
+	int destLen = 0;
+	int srcLen = 0;
+
+	while (dest[destLen] != '\0')destLen++;
+	
+	while (src[srcLen])
+	{
+		dest[destLen] = src[srcLen];
+		srcLen++;
+		destLen++;
+	}
+	dest[destLen] = '\0';
+
+	return (dest);
+}
